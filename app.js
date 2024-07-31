ec0000a7cae437fc64ba16b160531bbcaf78477b
Luis Balan <jlbalcz@gmail.com>
2024-07-30 20:28:49 -0500

    add example using writeFile

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
+
 console.clear();
+let output = "";
 const multiplicationTable = (number) => {
     for ( let i = 0; i <= 10; i++ ) {
-        console.log(`${number} X ${i} = ${number*i}`);
+        output += `${number} X ${i} = ${number*i}\n`;
     };
+    console.log(output)
+    
+    fs.writeFile('ten-table.txt', output, (err) => {
+        if (err) throw err;
+        console.log('File written successfully!', output)
+    })
 }
 
-multiplicationTable(3)
+multiplicationTable(10)
