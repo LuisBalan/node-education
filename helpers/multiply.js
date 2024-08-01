a346fab763121cdf922ab68319d871f8f669aa25
Luis Balan <jlbalcz@gmail.com>
2024-08-01 02:01:10 -0500

    fix for colors and list printed in prompt

--- helpers/multiply.js
+++ helpers/multiply.js
@@ -5,15 +5,21 @@ const colors = require('colors');
 const createFile = (basis, list, until) => {
     const filePromise = new Promise((resolve, reject) => {
         let output = "";
-        let header = `========== Multiplication table from ${basis} to ${until} ==========`.rainbow;
-        let footer = "====================================================================".rainbow;
+        let contentFile = "";
+        let headerContentFile = `========== ${basis}-Multiplication table until ${until} ==========`;
+        let footerContentFile = "====================================================================";
+        let header = `${`========== ${basis}-Multiplication table from  to ${until} ==========`.rainbow}`;
+        let footer = `${"====================================================================".rainbow}`;
         let fileName = `${basis}_multiplication_table_until_${until}.txt`
         output += header + `\n`;
+        contentFile += headerContentFile + `\n`;
         for (i = 0 ; i <= until ; i++ ) {
             output += `${basis} ${'X'.green} ${i} = ${basis*i}\n`
+            contentFile += `${basis} X ${i} = ${basis*i}\n`
         };
         output += footer + '\n';
-        fs.writeFile(fileName , output, (err) => {
+        contentFile += footerContentFile + '\n';
+        fs.writeFile(fileName , contentFile, (err) => {
             if (err) {
                 reject (err)
             } else {
