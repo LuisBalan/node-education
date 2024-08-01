2b00946212e2ac662d8e309ec8609a030a975068
Luis Balan <jlbalcz@gmail.com>
2024-07-31 22:18:39 -0500

    install yargs and recover params added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const { createFile } = require('./helpers/multiply');
+const argv = require('yargs').argv;
 
 
-const basis = Number(process.argv[2].substring(8));
-createFile(basis)
-.then( fileName => console.log(`${fileName} sucessfully created!`))
-.catch(err => console.log(err))
+// const basis = Number(process.argv[2].substring(8));
+// createFile(basis)
+// .then( fileName => console.log(`${fileName} sucessfully created!`))
+// .catch(err => console.log(err))
+
+console.log(process.argv)
+console.log(argv)
+console.log('base: yargs', argv.basis)
+console.log('limit yargs: ', argv.limite)
