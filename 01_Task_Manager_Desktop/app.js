d770744c56f39a7d79dbe55af00e3d4f4c86a3b3
LuisBalan <jlbalcz@gmail.com>
2024-08-06 22:19:08 -0600

    create classes Task and Tasks and test them

--- 01_Task_Manager_Desktop/app.js
+++ 01_Task_Manager_Desktop/app.js
@@ -1,15 +1,26 @@
 require('colors');
 const { showMenu, pause } = require('./helpers/messages');
 const { inquirerMenu, inquirerPause } = require('./helpers/inquirer');
+const Task = require('./models/Task')
+const _taskList = require('./models/Tasks');
+const Tasks = require('./models/Tasks');
 
 const main = async () => {
     let selectedOpt;
     let pause;
     do {
         selectedOpt = await inquirerMenu();
-        // console.log('selected opt: ', {selectedOpt});
+         console.log('selected opt: ', {selectedOpt});
 
         pause = await inquirerPause();
+
+        // const newTask = new Task('Buy food');
+        // console.log('-- new task: ', newTask)
+        // const tasks = new Tasks();
+        
+        // tasks._taskList[newTask.id] = newTask;
+        // console.log('--- tasks ', tasks)
+        // await pause();
         
     } while (
         selectedOpt !== '0'
